a26d866d0b170be1e908f5bc9cf65e5373e61aaa
ConnorAU <15099385+ConnorAU@users.noreply.github.com>
2025-04-29 08:03:49 +1000

    Add mod.cpp Adding to git for use in remote build workflow

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name="Extended Function Viewer";
+overview="An advanced function viewer with list filtering, popular themes, multiple file loading methods and syntax highlighting!";
+tooltipOwned="Extended Function Viewer";
+action="https://github.com/ConnorAU/A3ExtendedFunctionViewer";
+
+hideName=0;
+hidePicture=0;
+dlcColor[]={1,1,1,1};
